0bb82d7301bc54df4a93aa96ee9349d9c808b870
AntonyAkash <antoney97@gmail.com>
2024-03-15 08:26:56 +0530

    First Commit

---
+++ src/main/java/com/example/demo/CompleteStackProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteStackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteStackProjectApplication.class, args);
+		System.out.println("THank you God");
+	}
+
+}
